b9dcbbaaaf5b29d6f3fb96887f23675d8667c229
emrecoban <fbdarktole@gmail.com>
2023-11-04 16:14:15 +0300

    Added authorization

--- app/webhook/status/route.js
+++ app/webhook/status/route.js
@@ -1,4 +1,11 @@
+import { headers } from "next/headers";
+
 export async function POST(request) {
+  const headersList = headers();
+  const authorization = headersList.get("Authorization");
+
+  console.log("Signature secret: ", authorization);
+
   const res = await request.json();
   console.log("SMS Status: ", JSON.stringify(res, null, 2));
 
